3d7974e812670a52a82554cdca08942b516573cc
Aron Hidalgo <aron.hidalgo@ucr.ac.cr>
2025-04-12 19:07:43 -0600

    Agregar solución del laboratorio 4. Creación de bases de datos de países, conexión con backend, se agregan clases PaisesController.cs, PaisesHandler.cs y PaisModel.cs

---
+++ backend-lab-c23880/backend-lab-c23880/Controllers/PaisesController.cs
@@ -0,0 +1,26 @@
+﻿using backend_lab_c23880.Handlers;
+using backend_lab_c23880.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_lab_c23880.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisesController : ControllerBase
+    {
+        private readonly PaisesHandler _paisesHandler;
+
+        public PaisesController()
+        {
+            _paisesHandler = new PaisesHandler();
+        }
+        [HttpGet]
+        public List<PaisModel> Get()
+        {
+            var paises = _paisesHandler.ObtenerPaises();
+            return paises;
+        }
+
+    }
+}
---
+++ backend-lab-c23880/backend-lab-c23880/Models/PaisModel.cs
@@ -0,0 +1,11 @@
+﻿namespace backend_lab_c23880.Models
+{
+    public class PaisModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Continente { get; set; }
+        public string Idioma { get; set; }
+
+    }
+}
