595205299fa3b8a132b7045c0f3adb61f9f00896
Aron Hidalgo <aron.hidalgo@ucr.ac.cr>
2025-04-27 10:28:58 -0600

    Laboratorio #5: formulario completo, agregar países

--- backend-lab-c23880/backend-lab-c23880/Controllers/PaisesController.cs
+++ backend-lab-c23880/backend-lab-c23880/Controllers/PaisesController.cs
@@ -22,5 +22,25 @@ namespace backend_lab_c23880.Controllers
             return paises;
         }
 
+        [HttpPost]
+        public async Task<ActionResult<bool>> CrearPais(PaisModel pais) 
+        {   
+            try 
+            {
+                if (pais == null)
+                {
+                    return BadRequest();
+                }     
+
+                PaisesHandler paisesHandler = new PaisesHandler();
+                var resultado = paisesHandler.CrearPais(pais);
+                return new JsonResult(resultado);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error creando país");
+            }
+        }
+
     }
 }
